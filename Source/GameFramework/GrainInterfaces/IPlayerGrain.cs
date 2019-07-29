f2253e95c7d4fda4332f8573dbda4d80b002bdcb
bzh <381675273@qq.com>
2019-07-29 18:02:26 +0800

    battle server

---
+++ Source/GameFramework/GrainInterfaces/IPlayerGrain.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrainInterfaces
+{
+    public interface IPlayerGrain : Orleans.IGrainWithIntegerKey
+    {
+    }
+}
---
+++ Source/GameFramework/GrainInterfaces/IWorldGrain.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GrainInterfaces
+{
+    interface IWorldGrain : Orleans.IGrainWithIntegerKey
+    {
+    }
+}
